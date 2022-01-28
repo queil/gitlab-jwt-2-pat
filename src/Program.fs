c650306f1fc5a9779f8233bbf3ccc89f6fc7a68f
queil <queil@users.noreply.github.com>
2022-01-28 15:42:31 +0000

    Some implementation

--- src/Program.fs
+++ src/Program.fs
@@ -1,67 +1,61 @@
 module Queil.Gitlab.Jwt2Pat.App
 
 open System
-open System.IO
+open System.IdentityModel.Tokens.Jwt
+open System.Net.Http
+open System.Text.Json
+open Microsoft.AspNetCore.Authentication.JwtBearer
 open Microsoft.AspNetCore.Builder
-open Microsoft.AspNetCore.Cors.Infrastructure
-open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.Http
+open Microsoft.AspNetCore.Authentication
 open Microsoft.Extensions.Hosting
 open Microsoft.Extensions.Logging
 open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Configuration
 open Giraffe
+open FsHttp
+open FsHttp.DslCE
+open Microsoft.Extensions.Options
+open Microsoft.IdentityModel.Tokens
 
-// ---------------------------------
-// Models
-// ---------------------------------
+[<CLIMutable>]
+type GitlabOptions = {
+    hostname: string
+    apiKey: string
+}
 
-type Message =
-    {
-        Text : string
-    }
+let tokenHandler : HttpHandler  =
+   fun  (next : HttpFunc) (ctx : HttpContext) ->
 
-// ---------------------------------
-// Views
-// ---------------------------------
+        let opts = ctx.RequestServices.GetRequiredService<IOptions<GitlabOptions>>().Value
 
-module Views =
-    open Giraffe.ViewEngine
-
-    let layout (content: XmlNode list) =
-        html [] [
-            head [] [
-                title []  [ encodedText "src" ]
-                link [ _rel  "stylesheet"
-                       _type "text/css"
-                       _href "/main.css" ]
-            ]
-            body [] content
-        ]
-
-    let partial () =
-        h1 [] [ encodedText "Queil.Gitlab.Jwt2Pat" ]
-
-    let index (model : Message) =
-        [
-            partial()
-            p [] [ encodedText model.Text ]
-        ] |> layout
-
-// ---------------------------------
-// Web app
-// ---------------------------------
-
-let indexHandler (name : string) =
-    let greetings = sprintf "Hello %s, from Giraffe!" name
-    let model     = { Text = greetings }
-    let view      = Views.index model
-    htmlView view
+        task {
+            let! jwtString = ctx.GetTokenAsync("Bearer")
+            let jwt = JwtSecurityTokenHandler().ReadJwtToken(jwtString)
+            let userId = jwt.Payload["user_id"]
+            let expiresAt = jwt.ValidTo.ToIsoString()
+            let name = jwt.Subject
+            let result =
+                http {
+                   POST $"{opts.hostname}/api/v4/users/{userId}/impersonation_tokens"
+                   header "PRIVATE-TOKEN" opts.apiKey
+                   formUrlEncoded
+                     [
+                         "expires_at", expiresAt
+                         "name", name
+                     ]
+                }
+                |> Response.assertOk
+                |> Response.toStream
+            let! responseContent = JsonSerializer.DeserializeAsync<{|token:string|}>(result)
+            return! ctx.WriteStringAsync responseContent.token
+        }
 
 let webApp =
     choose [
         GET >=>
             choose [
-                route "/" >=> indexHandler "world"
-                routef "/hello/%s" indexHandler
+                route "/token" >=> requiresAuthentication (RequestErrors.UNAUTHORIZED "Bearer" "gitlab-pat" "") >=> tokenHandler
             ]
         setStatusCode 404 >=> text "Not Found" ]
 
@@ -77,39 +71,37 @@ let errorHandler (ex : Exception) (logger : ILogger) =
 // Config and Main
 // ---------------------------------
 
-let configureCors (builder : CorsPolicyBuilder) =
-    builder
-        .WithOrigins(
-            "http://localhost:5000",
-            "https://localhost:5001")
-       .AllowAnyMethod()
-       .AllowAnyHeader()
-       |> ignore
-
-
-let contentRoot = Directory.GetCurrentDirectory()
-let webRoot     = Path.Combine(contentRoot, "WebRoot")
-let builder = WebApplication.CreateBuilder(WebApplicationOptions(
-                ContentRootPath = Directory.GetCurrentDirectory(),
-                EnvironmentName = Environments.Staging,
-                WebRootPath = webRoot
-))
+let builder = WebApplication.CreateBuilder()
 let services = builder.Services
-services.AddAuthentication() |> ignore
+let jwtIssuer = builder.Configuration.GetValue("jwt:issuer")
+
+services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(fun opts ->
+            opts.BackchannelHttpHandler <- new HttpClientHandler()
+            opts.Authority <- jwtIssuer
+            opts.RequireHttpsMetadata <- false
+            opts.MetadataAddress <- $"{jwtIssuer}/.well-known/openid-configuration"
+            opts.TokenValidationParameters <- TokenValidationParameters(
+                ValidateIssuer = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = jwtIssuer
+            )
+        ) |> ignore
+services.Configure<GitlabOptions>(builder.Configuration.GetSection("gitlab")) |> ignore
 services.AddCors()    |> ignore
 services.AddGiraffe() |> ignore
+
 builder.Logging.AddConsole().AddDebug() |> ignore
 
 let app = builder.Build()
 
-match app.Environment.IsDevelopment() with
-| true  ->
-    app.UseDeveloperExceptionPage() |> ignore
-| false ->
-    app.UseGiraffeErrorHandler(errorHandler)
-       .UseHttpsRedirection() 
-       .UseCors(configureCors)
-       .UseStaticFiles()
-       .UseAuthentication()
-       .UseGiraffe(webApp)
+if app.Environment.IsDevelopment() then app.UseDeveloperExceptionPage() |> ignore
+
+app.UseGiraffeErrorHandler(errorHandler)
+   .UseHttpsRedirection() 
+   .UseStaticFiles()
+   .UseAuthentication()
+   .UseGiraffe(webApp)
+
 app.Run()
